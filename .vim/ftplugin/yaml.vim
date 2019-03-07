e623d9eac88c5dc8c597596418cbaeab85328e77
Blind Squirrel <squirrel289@calan.co>
2019-03-07 16:10:58 -0500

    Moved vim files to .vim directory. Added init.sh 	new file:   .vim/ftdetect/cloudformation.vim 	new file:   .vim/ftplugin/cloudformation.vim 	new file:   .vim/ftplugin/yaml.vim 	new file:   init.sh 	deleted:    .vim-ftdetect/cloudformation.vim 	deleted:    .vim-ftplugin/cloudformation.vim 	deleted:    .vim-ftplugin/yaml.vim

---
+++ .vim/ftplugin/yaml.vim
@@ -0,0 +1,1 @@
+let b:ale_linter_aliases = {'yaml': ['yaml']} 
---
+++ init.sh
@@ -0,0 +1,6 @@
+ln -s ~/.dotfiles/.gitconfig ~/
+ln -s ~/.dotfiles/.bash_profile ~/
+ln -s ~/.dotfiles/.profile ~/
+ln -s ~/.dotfiles/.vimrc ~/
+ln -s ~/.dotfiles/.vim/* ~/.vim/
+ln -s ~/.dotfiles/.config ~/
